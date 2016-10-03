ae7711416883b892c5570a1b151d34678fe5b7e4
drack00 <uncooldan@live.ca>
2016-10-03 20:30:14 -0300

    Added some wave functionality.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,33 +4,187 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController singleton
+    {
+        get
+        {
+            return FindObjectOfType<GameController>();
+        }
+    }
+
     [System.Serializable]
     public class Wave
     {
-        [System.Serializable]
-        public enum TimeLimit
-        {
-            None, Fail, Succeed
-        }
-        public TimeLimit useTimeLimit;
+        public bool useTimeLimit;
         public float timeLimit;
-        private float _timeLimit;
 
-        public int minTargets;
-        public GameObject[] targetPrefabs;
-        private List<GameObject> targets;
-        public GameObject[] otherPrefabs;
-        private List<GameObject> others;
+        public AnimationCurve spawnCurve;
+        public float spawnRate;
+        private float spawnIndex;
+        [System.Serializable]
+        public enum SpawnMethod
+        {
+            Roundrobin, Curve, Weighted, Random
+        }
+        public GameObject[] prefabs;
+        public SpawnMethod prefabsMethod;
+        public AnimationCurve prefabsCurve;
+        private int prefabsIndex;
+        private GameObject currentPrefab
+        {
+            get
+            {
+                switch(prefabsMethod)
+                {
+                    case SpawnMethod.Roundrobin:
+                        while (prefabsIndex >= prefabs.Length)
+                            prefabsIndex -= prefabs.Length;
+                        return prefabs[prefabsIndex];
+                    case SpawnMethod.Curve:
+                        return prefabs[Mathf.FloorToInt(prefabsCurve.keys[prefabsIndex].value)];
+                    case SpawnMethod.Weighted:
+                        List<GameObject> odds = new List<GameObject>();
+                        for(int i = 0; i < prefabsCurve.keys.Length; i++)
+                        {
+                            for(int j = 0; j < Mathf.FloorToInt(prefabsCurve.keys[i].value); j++)
+                            {
+                                odds.Add(prefabs[i]);
+                            }
+                        }
+                        return odds[Random.Range(0, odds.Count)];
+                    case SpawnMethod.Random:
+                        return prefabs[Random.Range(0, prefabs.Length)];
+                }
 
-        public AnimationCurve staggerSpawns;
-        public float spawnCoefficent;
+                return null;
+            }
+        }
+        public Transform[] points;
+        public SpawnMethod pointsMethod;
+        public AnimationCurve pointsCurve;
+        private int pointsIndex;
+        private Transform currentPoint
+        {
+            get
+            {
+                switch (pointsMethod)
+                {
+                    case SpawnMethod.Roundrobin:
+                        while (pointsIndex >= points.Length)
+                            pointsIndex -= points.Length;
+                        return points[pointsIndex];
+                    case SpawnMethod.Curve:
+                        return points[Mathf.FloorToInt(pointsCurve.keys[pointsIndex].value)];
+                    case SpawnMethod.Weighted:
+                        List<Transform> odds = new List<Transform>();
+                        for (int i = 0; i < pointsCurve.keys.Length; i++)
+                        {
+                            for (int j = 0; j < Mathf.FloorToInt(pointsCurve.keys[i].value); j++)
+                            {
+                                odds.Add(points[i]);
+                            }
+                        }
+                        return odds[Random.Range(0, odds.Count)];
+                    case SpawnMethod.Random:
+                        return points[Random.Range(0, points.Length)];
+                }
+
+                return null;
+            }
+        }
+
+        public GameObject Spawn ()
+        {
+            GameObject go = (GameObject)Instantiate(currentPrefab, currentPoint.position, currentPoint.rotation);
+
+            prefabsIndex++;
+            pointsIndex++;
+
+            return go;
+        }
+
+        public List<GameObject> gos;
+        private bool areAllSpawned
+        {
+            get
+            {
+                int totalSpawns = 0;
+                foreach (Keyframe key in spawnCurve.keys)
+                {
+                    totalSpawns += Mathf.RoundToInt(key.value);
+                }
+                return gos.Count >= totalSpawns;
+            }
+        }
+        private bool isWaveComplete
+        {
+            get
+            {
+                bool _isWaveComplete = true;
+                foreach (GameObject go in gos)
+                {
+                    if(go.activeSelf)
+                    {
+                        _isWaveComplete = false;
+                        break;
+                    }
+                }
+                return _isWaveComplete && areAllSpawned;
+            }
+        }
 
         public bool despawnOnEnd;
 
-        public IEnumerator Update ()
+        public IEnumerator Update (List<GameObject> _gos = null)
         {
-            yield return null;
+            gos = new List<GameObject>();
+            if (_gos != null)
+                foreach (GameObject go in _gos)
+                {
+                    gos.Add(go);
+                }
+
+            pointsIndex = 0;
+            prefabsIndex = 0;
+            spawnIndex = 0.0f;
+            int lastSpawnIndex = 0;
+
+            interrupt = false;
+            while(!interrupt && (!useTimeLimit || spawnIndex < timeLimit) && !isWaveComplete)
+            {
+                int currentSpawnIndex = Mathf.FloorToInt(spawnIndex);
+                if (spawnIndex == 0.0f || currentSpawnIndex != lastSpawnIndex)
+                {
+                    int currentSpawn = Mathf.FloorToInt(spawnCurve.keys[currentSpawnIndex].value);
+
+                    for (int i = 0; i < currentSpawn; i++)
+                    {
+                        gos.Add(Spawn());
+                    }
+
+                    lastSpawnIndex = currentSpawnIndex;
+                }
+
+                yield return null;
+
+                spawnIndex = spawnRate * Time.deltaTime;
+            }
+
+            if(despawnOnEnd)
+                foreach (GameObject go in gos)
+                {
+                    go.SetActive(false);
+                }
+
+            foreach (GameObject go in gos)
+            {
+                if (!go.activeSelf)
+                    Destroy(go);
+            }
+
+            GameController.singleton.currentWave = GameController.singleton.nextWave;
         }
+        [HideInInspector]public bool interrupt;
     }
     public Wave[] waves;
     private int currentWaveIndex
@@ -53,15 +207,22 @@ public class GameController : MonoBehaviour
         {
             return _currentWave;
         }
-        private set
+        set
         {
             if (_currentWave != null)
                 StopCoroutine(_currentWave.Update());
 
-            _currentWave = value;
+            if (value != null)
+            {
+                if (_currentWave != null)
+                    StartCoroutine(_currentWave.Update(_currentWave.gos));
+                else
+                    StartCoroutine(_currentWave.Update());
+            }
+            else
+                EndGame();
 
-            if (_currentWave != null)
-                StartCoroutine(_currentWave.Update());
+            _currentWave = value;
         }
     }
     private Wave nextWave
