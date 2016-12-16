9879a43df6d03bfc5fd851ca4c3255f680118f5d
drack00 <uncooldan@live.ca>
2016-12-16 00:46:13 -0400

    Got rid of forward oriented movement.
    Not a requirement anymore since orientation is now handled seperately.
    Issue smoothing movement AI often overshoots destination, need to figure
    out a way to add braking.

--- Assets/Scripts/AIControlled_MovingObject.cs
+++ Assets/Scripts/AIControlled_MovingObject.cs
@@ -19,45 +19,24 @@ public class AIControlled_MovingObject : MovingObject
         ai.movement.agent.updateRotation = false;
         ai.movement.agent.Stop();
     }
+
+    public float acceleration;
     public float maxSpeed;
+
     public virtual void FixedUpdate()
     {
         GroundCheck();
-        Vector3 navDirection = (ai.movement.agent.destination - transform.position).normalized;
-        //movementSettings.UpdateDesiredTargetSpeed(navDirection);
+        Vector3 navDirection = ai.movement.agent.destination - transform.position;
 
         if ((navDirection.sqrMagnitude > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
         {
-            float desiredDir = MathStuff.SignedAngleBetween(transform.forward, navDirection, Vector3.up) * Mathf.Rad2Deg;
-            rigidbody.AddTorque(new Vector3(0.0f, (-1 * rigidbody.angularVelocity.y) + desiredDir, 0.0f), ForceMode.Impulse);
-
-            Vector3 desiredMove = transform.forward * maxSpeed;
-            rigidbody.AddForce((-1 * rigidbody.velocity) + desiredMove, ForceMode.Impulse);
+            Vector3 desiredForce = navDirection.normalized * acceleration;
+            Vector3 sumForce = rigidbody.velocity + desiredForce;
+            Vector3 force = Vector3.ClampMagnitude(sumForce, maxSpeed) - rigidbody.velocity;
+            rigidbody.AddForce(force, ForceMode.Acceleration);
         }
     }
 
-    /*[System.Serializable]
-    public class MovementSettings
-    {
-        public float ForwardSpeed = 8.0f;
-        public float BackwardSpeed = 4.0f;
-        public float StrafeSpeed = 4.0f;
-        public float JumpForce = 30f;
-        public AnimationCurve SlopeCurveModifier = new AnimationCurve(new Keyframe(-90.0f, 1.0f), new Keyframe(0.0f, 1.0f), new Keyframe(90.0f, 0.0f));
-        [HideInInspector]public float CurrentTargetSpeed = 8f;
-
-        public void UpdateDesiredTargetSpeed(Vector3 input)
-        {
-            if (input == Vector3.zero) return;
-            if (input.x > 0 || input.x < 0)
-                CurrentTargetSpeed = StrafeSpeed;
-            if (input.y < 0)
-                CurrentTargetSpeed = BackwardSpeed;
-            if (input.y > 0)
-                CurrentTargetSpeed = ForwardSpeed;
-        }
-    }
-    public MovementSettings movementSettings = new MovementSettings();*/
     [System.Serializable]
     public class AdvancedSettings
     {
@@ -74,30 +53,18 @@ public class AIControlled_MovingObject : MovingObject
             return GetComponent<CapsuleCollider>();
         }
     }
-    //private Vector3 m_GroundContactNormal;
     private bool m_Jump, m_PreviouslyGrounded, m_Jumping, m_IsGrounded;
     private void GroundCheck()
     {
         m_PreviouslyGrounded = m_IsGrounded;
         RaycastHit hitInfo;
         if (Physics.SphereCast(transform.position, m_Capsule.radius, Vector3.down, out hitInfo, ((m_Capsule.height / 2f) - m_Capsule.radius) + advancedSettings.groundCheckDistance))
-        {
             m_IsGrounded = true;
-            //m_GroundContactNormal = hitInfo.normal;
-        }
         else
-        {
             m_IsGrounded = false;
-            //m_GroundContactNormal = Vector3.up;
-        }
         if (!m_PreviouslyGrounded && m_IsGrounded && m_Jumping)
             m_Jumping = false;
     }
-    /*private float SlopeMultiplier()
-    {
-        float angle = Vector3.Angle(m_GroundContactNormal, Vector3.up);
-        return movementSettings.SlopeCurveModifier.Evaluate(angle);
-    }*/
 }
  
  
--- Assets/Scripts/AI_Action.cs
+++ Assets/Scripts/AI_Action.cs
@@ -6,13 +6,15 @@ public class AI_Action : MonoBehaviour {
     [System.Serializable]
     public enum Subroutine
     {
-        Masked, None,
+        Masked, None, Attack
     }
 
     public interface ISubroutine
     {
         void Do(AI_Action action);
     }
+
+    public Animator animator;
 
     [System.Serializable]
     public class Default : ISubroutine
@@ -23,6 +25,16 @@ public class AI_Action : MonoBehaviour {
         }
     }
     public Default defaultSubroutine;
+
+    [System.Serializable]
+    public class Attack : ISubroutine
+    {
+        public void Do(AI_Action action)
+        {
+            action.animator.SetTrigger("Attack");
+        }
+    }
+    public Attack attackSubroutine;
 
     [HideInInspector]
     public Subroutine selectedSubroutine;
@@ -55,7 +67,9 @@ public class AI_Action : MonoBehaviour {
 
         switch (selectedSubroutine)
         {
-
+            case Subroutine.Attack:
+                activeSubroutine = attackSubroutine;
+                break;
         }
 
         activeSubroutine.Do(this);
