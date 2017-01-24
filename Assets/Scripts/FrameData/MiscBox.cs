0b384c43b106889424a4ce5fe6ab4f6291ff391f
drack00 <uncooldan@live.ca>
2017-01-24 00:01:33 -0400

    Failsafe to how blocking masks work.
    There was prviously a loop hole where if multiple overlapping additive
    blocking masks were active and one was disabled that one would take
    precedence. This shouldn't happen any more.

--- Assets/Scripts/FrameData/MiscBox.cs
+++ Assets/Scripts/FrameData/MiscBox.cs
@@ -26,7 +26,9 @@ public class MiscBox : ActiveFrameData
     }
     public void Update ()
     {
-        if (!additive)
+        if (additive)
+            mo.blockingMask |= blockingMask;
+        else
             mo.blockingMask = blockingMask;
 
         updateBehaviour.Do(true, this);
