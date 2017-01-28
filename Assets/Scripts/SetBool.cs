99d38ee0a7ab3d54828e29083dc0ecf7fd573de2
drack00 <uncooldan@live.ca>
2017-01-27 20:19:34 -0400

    Corrected more animation logic issue.
    Charge moves in an inactive moveset can no longer transition to strong
    moves.

---
+++ Assets/Scripts/SetBool.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBool : MonoBehaviour
+{
+    public Animator animator;
+    public string boolName;
+    void OnEnable()
+    {
+        animator.SetBool(boolName, true);
+    }
+    void OnDisable()
+    {
+        animator.SetBool(boolName, false);
+    }
+}
